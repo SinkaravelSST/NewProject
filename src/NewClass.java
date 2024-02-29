9e50f686ef67692de9b02ccd7f96365d21278ab2
SinkaravelSST <sinkaravel.it@gmail.com>
2024-02-29 11:40:33 +0530

    Junior Code Updated

--- src/NewClass.java
+++ src/NewClass.java
@@ -7,6 +7,10 @@ public class NewClass {
 		System.out.println("Step 1");
 		System.out.println("Step 2");
 		System.out.println("Step 3");
+		
+		// Junior
+		System.out.println("Step 6");
+		System.out.println("Step 7");
 	}
 
 }
